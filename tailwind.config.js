1467437d7fcc52123efc7c794dc7ae7e5b4b1ffc
mrunalsen <mrunalpatel2500@gmail.com>
2022-10-14 11:29:06 +0530

    Secondary Commit: Crud complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  mode: 'jit',
   content: [
     './src/**/*.{html,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: colors.rose,
+      },
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
